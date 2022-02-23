ee9ac5218f013daa7dbfa4b3ad9613e5638b3525
Le Trong Dat <datlt2306@gmail.com>
2022-02-23 17:22:56 +0700

    Update

---
+++ server/data.js
@@ -0,0 +1,10 @@
+export const books = [
+    {id: 1, name: "Book A", authorId: 1},
+    {id: 2, name: "Book B", authorId: 1},
+    {id: 3, name: "Book C", authorId: 2},
+    {id: 4, name: "Book D", authorId: 2},
+]
+export const authors = [
+    {id: 1, name: "Dat", age: 20},
+    {id: 2, name: "Kien", age: 30}
+]
---
+++ server/schema/schema.js
@@ -0,0 +1,26 @@
+import { gql } from 'apollo-server-express';
+const typeDefs = gql`
+    type Book {
+        id: ID
+        name: String
+        genre: String
+        author: Author
+    }
+
+    type Author{
+        id: ID!
+        name: String
+        age: Int
+        books: [Book]
+    }
+
+    # ROOT TYPE
+    type Query {
+        books: [Book]
+        book(id: ID!): Book
+        authors: [Author]
+        author(id: ID!): Author
+    }
+`
+
+export default typeDefs;
